8f96b520855401f9fde55f1577dc8a816dbc1ede
burghausen8 <burghausen8@gmail.com>
2024-06-18 19:57:23 -0300

    feat: implement modal route

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/pages/product_detail_page.dart';
+import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
   final Product product;
@@ -12,24 +14,39 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        child: Image.network(
-          product.imageUrl,
-          fit: BoxFit.cover,
-        ),
-        footer: GridTileBar(
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pushNamed(
+            AppRoutes.PRODUCT_DETAIL,
+            arguments: product,
+          );
+        },
+        child: GridTile(
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
           ),
-          backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: null,
-          ),
-          trailing: IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: null,
+          footer: GridTileBar(
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+            ),
+            backgroundColor: Colors.black87,
+            leading: IconButton(
+              color: Theme.of(context).cardColor,
+              icon: Icon(
+                Icons.favorite,
+                color: Theme.of(context).cardColor,
+              ),
+              onPressed: null,
+            ),
+            trailing: IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Theme.of(context).cardColor,
+              ),
+              onPressed: null,
+            ),
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
+import 'package:shop/utils/app_routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,26 +16,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+        primarySwatch: Colors.purple,
+        cardColor: Colors.deepOrange,
+        fontFamily: 'Lato',
         useMaterial3: false,
       ),
       home: ProductsOverviewPage(),
       debugShowCheckedModeBanner: false,
+      routes: {
+        AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailPage(),
+      },
     );
   }
 }
